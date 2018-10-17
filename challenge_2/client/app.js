72a93de07389c16589cbf9a8b96137d92962532b
Wilson Li <wilsonli415@gmail.com>
2018-10-16 17:24:30 -0700

    setup server and html and posts data as csv

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,31 @@
 
 
+var generateCsv = (obj, keys = []) => {
+  if (keys.length === 0) {
+    for (var key in obj) {
+      if (key !== 'children') {
+        keys.push(key);
+      }
+    }
+    var csv = keys.join() + '\n';
+  }
+
+  var array = [];
+  for (var key of keys) {
+    array.push(obj[key]);
+  }
+
+  if (csv === undefined) {
+       csv = '';  
+   }
+  csv += array.join() + '\n';
+
+  if (obj.children.length > 0) {
+    for (var i = 0; i < obj.children.length; i++) {
+      csv += generateCsv(obj.children[i], keys);
+    }
+  }
+  return csv;
+};
+
+module.exports.generateCsv = generateCsv;
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var generateCsv = require('./client/app.js');
 
+var app = express()
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+app.use(express.static(path.join(__dirname, '/client')));
+ 
+
+app.post('/csv', function (req, res) {
+  var data = generateCsv.generateCsv(JSON.parse(req.body.json_text))
+  console.log(data)
+  res.send(data);
+})
+ 
+app.listen(3000, () => console.log(`Listening on port 3000!`));
 
